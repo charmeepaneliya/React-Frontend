581bf18602b1fef99eb8d9d5f17bb1d95a6691b5
Charmee Paneliya <cpjstudy28@gmail.com>
2026-09-10 13:10:51 +0530

    add some changes

--- 04_useState/useState/src/components/objectUseState.jsx
+++ 04_useState/useState/src/components/objectUseState.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 
 const ObjectUseState = () => {
+  // Create a state variable "user" with email and password properties 
+  // setUser is used to update the user state
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
   const hendleChange = (e) => {
+    // Get the name of the input field 
+    // It will be either "email" or "password"
     const field = e.target.name;
+    // Update the user object
     setUser((prev) => ({
       ...prev,
-      [field]: e.target.value,
+
+      [field]: e.target.value,  //Update only the property whose name matches the input field
     }));
   };
 
